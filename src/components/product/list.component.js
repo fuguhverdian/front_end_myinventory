c6f1c808b07aeae94f9a709c426a268f848089aa
fuguhverdian <fahrulfuguhv@students.amikom.ac.id>
2025-12-11 23:56:35 +0700

    perbaikan

--- src/components/product/list.component.js
+++ src/components/product/list.component.js
@@ -77,7 +77,7 @@ setProducts(response.data);
                     </thead>
                     <tbody>
                         {
-                            products.length >= 0 && (products.map((row, key)=>(
+                            products.length > 0 && (products.map((row, key)=>(
                             <tr key={key}>
                             <td>{key+1}</td>
                             <td>{row.name}</td>
